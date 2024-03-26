2cab27b99ef946e9e8e3f62050a7fe3da8686965
pliu55 <pliu55@wisc.edu>
2024-03-26 15:47:10 -0500

    use SummarizedExperiment as output

--- tests/testthat/test-ppInp.R
+++ tests/testthat/test-ppInp.R
@@ -1,22 +1,37 @@
-require(magrittr)
+suppressMessages(require(SummarizedExperiment))
 
 main <- function() {
     testPpCnInp()
     testPpRnaInp()
+    testPpRealInp()
 
     testPpPermInp()
+}
+
+testPpRealInp <- function() {
+    fcn = system.file('extdata/TcgaInp/focal_tumor.rds', package='MPAC')
+    ftumor = system.file('extdata/TcgaInp/log10fpkmP1_tumor.rds',package='MPAC')
+    fnorm = system.file('extdata/TcgaInp/log10fpkmP1_normal.rds',package='MPAC')
+    fcmp = system.file('extdata/TcgaInp/inp_real.rds', package='MPAC') 
+   
+    cn_tumor_mat = readRDS(fcn)
+    rna_tumor_mat = readRDS(ftumor)
+    rna_norm_mat  = readRDS(fnorm)
+   
+    out_se = ppRealInp(cn_tumor_mat, rna_tumor_mat, rna_norm_mat)
+    cmp_se = readRDS(fcmp)
+
+    test_that('ppRealInp', {expect_identical(out_se, cmp_se)})
 }
 
 testPpCnInp <- function() {
     fin  = system.file('extdata/TcgaInp/focal_tumor.rds', package='MPAC')
     fcmp = system.file('extdata/TcgaInp/inp_focal.rds',   package='MPAC')
-    inmat = readRDS(fin)
+    out_se = readRDS(fin) |> ppCnInp()
 
-    outmat = ppCnInp(inmat)
+    cmp_se = readRDS(fcmp)
 
-    cmpmat = readRDS(fcmp)
-
-    test_that('ppCnInp', {expect_identical(outmat, cmpmat)})
+    test_that('ppCnInp', {expect_identical(out_se, cmp_se)})
 }
 
 testPpRnaInp <- function() {
@@ -26,23 +41,19 @@ testPpRnaInp <- function() {
 
     tumor_mat = readRDS(ftumor)
     norm_mat  = readRDS(fnorm)
-    outmat = ppRnaInp(tumor_mat, norm_mat)
+    out_se = ppRnaInp(tumor_mat, norm_mat)
 
-    cmpmat = readRDS(fcmp)
+    cmp_se = readRDS(fcmp)
 
-    test_that('ppRnaInp', {expect_identical(outmat, cmpmat)})
+    test_that('ppRnaInp', {expect_identical(out_se, cmp_se)})
 }
 
 testPpPermInp <- function() {
-    fin_cn  = system.file('extdata/TcgaInp/inp_focal.rds', package='MPAC')
-    fin_rna = system.file('extdata/TcgaInp/inp_log10fpkmP1.rds',
-        package='MPAC')
-   
-    real_cn_mat  = readRDS(fin_cn)
-    real_rna_mat = readRDS(fin_rna)
+    freal = system.file('extdata/TcgaInp/inp_real.rds', package='MPAC') 
+    real_se = readRDS(freal)
     n_perms = 3
     set.seed(123456)
-    outll = ppPermInp(real_cn_mat, real_rna_mat, n_perms)
+    outll = ppPermInp(real_se, n_perms)
 
     fcmp = system.file('extdata/TcgaInp/inp_perm.rds', package='MPAC')
     cmpll = readRDS(fcmp)
