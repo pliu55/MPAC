d21ec8dff24841d6558e8036fa280f3610134a0e
pliu55 <pliu55@wisc.edu>
2024-03-31 20:28:38 -0500

    use SummarizedExperiment obj instead of mat

--- tests/testthat/test-pltNeiStt.R
+++ tests/testthat/test-pltNeiStt.R
@@ -5,17 +5,15 @@ main <- function() {
 testPltNeiStt <- function() {
     fpth = system.file('extdata/Pth/tiny_pth.txt', package='MPAC')
 
-    fcn  = system.file('extdata/pltNeiStt/inp_focal.rds',       package='MPAC')
-    frna = system.file('extdata/pltNeiStt/inp_log10fpkmP1.rds', package='MPAC')
-    fflt = system.file('extdata/pltNeiStt/fltmat.rds',          package='MPAC')
+    freal = system.file('extdata/pltNeiStt/inp_real.rds', package='MPAC')
+    fflt  = system.file('extdata/pltNeiStt/fltmat.rds',   package='MPAC')
 
-    cn_state_mat  = readRDS(fcn)
-    rna_state_mat = readRDS(frna)
+    real_se = readRDS(freal)
     fltmat = readRDS(fflt)
     protein = 'CD86'
 
     test_that('testPltNeiStt', {
-        pltNeiStt(cn_state_mat, rna_state_mat, fltmat, fpth, protein) |>
+        pltNeiStt(real_se, fltmat, fpth, protein) |>
         expect_no_error()
     })
 
